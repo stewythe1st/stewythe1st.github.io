168c0e9f87ab4f48e2904c3663ed3fe405d26d55
Stuart Miller <stewythe1st@gmail.com>
2022-08-16 11:18:34 -0500

    Many changes from the past day or so. Getting close to done.

--- style/script.js
+++ style/script.js
@@ -32,26 +32,4 @@ window.addEventListener("scroll", reveal);
 // To check the scroll position on page load
 reveal();
 
-function draw(x, y, circleX, circleY) {
-    const radius = 5;
-    const ctx = document.getElementById("aboutme-canvas").getContext("2d");
-    ctx.strokeStyle = "#000";
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    var direction = 1
-    if (circleY > y) {
-        direction = -1
-    }
-    ctx.lineTo(circleX - Math.abs(circleY - y) - (radius * direction) + radius, y);
-    ctx.lineTo(circleX, circleY - radius + (radius / 2 * direction));
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(circleX + radius / 2, circleY-radius,radius, 0, Math.PI * 2, true);
-    ctx.stroke();
-}
-draw(10, 40, 70, 15)
-draw(10, 45, 70, 30)
-draw(10, 50, 70, 45)
-draw(10, 55, 90, 70)
-draw(10, 60, 70, 75)
+drawCircuit("aboutme-canvas");
